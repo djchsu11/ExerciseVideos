1cb5077c280f93c46c7702a4907b02d7ded37e33
messenger <or.no.world@gmail.com>
2017-06-02 13:51:50 -0500

    Added initial models

--- Workout/admin.py
+++ Workout/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Patient, Exercise
 # Register your models here.
+
+admin.site.register(Patient)
+admin.site.register(Exercise)
--- Workout/models.py
+++ Workout/models.py
@@ -4,3 +4,20 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Patient(models.Model):
+    patient_id = models.CharField(max_length=36)
+    workout_start_date = models.DateTimeField('date started')
+    workout_end_date = models.DateTimeField('date finished')
+
+    def __str__(self):
+        return self.patient_id
+
+class Exercise(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    exercise_name = models.CharField(max_length=200)
+    video_link = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.exercise_name
+
