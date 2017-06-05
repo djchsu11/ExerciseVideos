4135000c1ba1030ccd4b188a1c8d92231833ae5f
= <=>
2017-06-04 22:18:00 -0500

    scaffolded out some of the layout

--- Workout/views.py
+++ Workout/views.py
@@ -9,9 +9,9 @@ from Workout.models import Patient
 
 
 def Index(request):
-    return render(request, 'workout/index.html')
+    return render(request, 'workout/index.html', {'active_tab':'home'})
 
 def Workout(request, patient_id):
     patient = get_object_or_404(Patient, pk=patient_id)
     print patient
-    return render(request, 'workout/workout.html', {'patient' : patient})
+    return render(request, 'workout/workout.html', {'patient' : patient, 'active_tab': 'workout'})
